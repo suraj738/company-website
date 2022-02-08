8c8612c886070544274f7854d282009bc2a41d74
suraj738 <kolisuraj4044@gmail.com>
2022-02-08 21:43:35 +0530

    added validation

--- src/components/Pages/Contact/index.js
+++ src/components/Pages/Contact/index.js
@@ -1,4 +1,29 @@
 function Contact(){
+    const fname;
+    const lname;
+    const email;
+    const mobile;
+    const subject;
+    const comment;
+     
+   const validate = () => {
+        if(fname.value==""){
+            return false    
+        }else if(lname.value==""){
+            return false 
+        }else if(email.value==""){
+            return false 
+        }else if(mobile.value==""){
+            return false 
+        }else if(subject.value==""){
+            return false 
+        }else if(comment.value==""){
+            return false 
+        }else{
+        return true
+    }
+        return false;
+      }
     return(
         <h1>Contact</h1>
     );
