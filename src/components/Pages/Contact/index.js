3ea112a7043f1220f2fdcf52d60bfd90dfd4728f
viki0302 <rajatlokhande03@gmail.com>
2022-02-08 21:45:58 +0530

    created form

--- src/components/Pages/Contact/index.js
+++ src/components/Pages/Contact/index.js
@@ -1,6 +1,29 @@
-function Contact(){
-    return(
-        <h1>Contact</h1>
+function Contact() {
+    return (
+        <>
+            <form autoComplete="off">
+                <div>
+                    <labal htmlfor=""> First Name :</labal>
+                    <input type="text" name="" value=""  />
+                    <label htmlFor=""> Last Name :</label>
+                    <input type="text" name="" value="" />
+                </div>
+                <div>
+                    <labal htmlfor=""> Mobile No :</labal>
+                    <input type="text" name="" value="" />
+                    <label htmlFor=""> Email :</label>
+                    <input type="text" name="" value="" />
+                </div>
+                <div>
+                    <labal htmlfor=""> Subject :</labal>
+                    <input type="text" name="" value="" />
+                    <label htmlFor=""> Comment :</label>
+                    <input type="text" name="" value="" />
+                </div>
+                <button type="submit"> Sumbit</button>
+            </form>
+        </>
+
     );
 };
 export default Contact; 
